6298ac5c9250a24d055505978201b4f0dedd44c6
IiroValkonenMooc <62344585+IiroValkonenMooc@users.noreply.github.com>
2020-08-29 00:04:58 +0300

    app.js siivottu

--- blog-list/bloglist-frontend/src/App.js
+++ blog-list/bloglist-frontend/src/App.js
@@ -37,18 +37,12 @@ import {
 } from 'react-router-dom'
 
 const App = () => {
-  const [ablogs, setBlogs] = useState([])
-  //const [token, setToken] = useState(null)
-  //const [loggedInName, setLoggedInName] = useState(null)
-  //const [loggedInUserName, setloggedInUsername] = useState(null)
-  // const [showMessage, setShowMessage] = useState(false)
-  // const [messageText, setMessageText] = useState(false)
-  // const [messageRed, setMessageRed] = useState(false)
   const blogFormRef = useRef()
 
   const dispatch = useDispatch()
   const blogs = useSelector(state => state.blogs, shallowEqual )
   const token = useSelector(state => state.token)
+  const users = useSelector(state => state.users)
 
   useEffect(() => {
     const lsName = localStorage.getItem('name')
@@ -56,21 +50,14 @@ const App = () => {
     const lsToken = localStorage.getItem('token')
 
     if( lsName && lsUsername && lsToken){
-      //setLoggedInName(lsName)
       dispatch( setTokenDataName(lsName) )
-      //setloggedInUsername(lsUsername)
       dispatch( setTokenDataUsername(lsUsername) )
-      //setToken(lsToken)
       dispatch( setTokenDataPayload(lsToken) )
     }
   }, [])
 
 
   useEffect(() => {
-    // blogService.getAll().then(blogs =>
-    //   setBlogs( blogs )
-    // )
-
     dispatch( setInitialBlogs() )
   }, [])
 
@@ -80,11 +67,8 @@ const App = () => {
 
     if(tokenData.err ===  null){
       handleMessageChange('Login successful')
-      //setLoggedInName(tokenData.login.name)
       localStorage.setItem('name', tokenData.login.name)
-      //setloggedInUsername(tokenData.login.username)
       localStorage.setItem('username', tokenData.login.username)
-      //setToken('bearer '+tokenData.login.token)
       localStorage.setItem('token', 'bearer '+tokenData.login.token)
       dispatch( setTokenData(tokenData.login) )
     } else {
@@ -94,9 +78,6 @@ const App = () => {
   }
 
   const handleLogout = () => {
-    // setLoggedInName(null)
-    // setloggedInUsername(null)
-    // setToken(null)
     dispatch( setTokenDataName('') )
     dispatch( setTokenDataUsername('') )
     dispatch( setTokenDataPayload('') )
@@ -110,19 +91,6 @@ const App = () => {
     console.log('newBlog :>> ', newBlog)
     console.log('token.payload :>> ', token.payload)
     dispatch( addBlog( token.payload, newBlog ) )
-    // const response = await blogService.submitBlog(token.payload, newBlog.title, newBlog.author, newBlog.url)
-
-    // console.log('response :>> ', response);
-
-    // if (response.err === null) {
-    //   handleMessageChange('New blog added: ' + newBlog.title)
-    //   const newBlogs = await blogService.getAll()
-    //   setBlogs(newBlogs)
-
-    //   blogFormRef.current.toggleVisibility()
-    // } else {
-    //   handleMessageChange('Blog to add new blog: ' + response.err.data.error)
-    // }
   }
 
   const handleMessageChange = async (text, red, timeoutDur) => {
@@ -149,108 +117,81 @@ const App = () => {
   }
 
   const handleBlogLike = async (blog) => {
-    // const response = await blogService.likeBlog(token.payload, blog)
-    // console.log('blog :>> ', blog)
-    // console.log('blog liked')
-
-    // if (response.err === null) {
-    //   handleMessageChange(`Blog: ${blog.title} liked`)
-    //   const newBlogs = await blogService.getAll()
-    //   setBlogs(newBlogs)
-
-    // } else {
-    //   handleMessageChange('Failed to add new blog: ' + response.err.response.data.error, true)
-    // }
     dispatch( likeBlog(token.payload, blog) )
   }
 
   const handleBlogDelete = async (blog) => {
-    // console.log('deleting blog')
-    // const confirmResult = window.confirm(`Really delete blog: ${blog.title}`)
-
-    // if (confirmResult) {
-
-    //   const response = await blogService.deleteBlog(token.payload, blog)
-
-    //   if (response.err === null) {
-    //     handleMessageChange(`Blog: ${blog.title} deleted`, false, 6000)
-    //     const newBlogs = await blogService.getAll()
-    //     setBlogs(newBlogs)
-    //   } else {
-    //     handleMessageChange('Failed to delete blog: ' + response.err.response.data.error, true, 6000)
-    //     const newBlogs = await blogService.getAll()
-    //     setBlogs(newBlogs)
-    //   }
-
-    // }
     dispatch( deleteBlog(token.payload, blog) )
   }
+
+  const idMatch = useRouteMatch('/users/:id')
 
   return (
     <div>
       <div className='Padded-element'>
         <h2>blogs</h2>
       </div>
-      <Router>
-        <LinkBar />
+      <LinkBar />
 
-        <Switch>
-          <Route path="/blogs">
+      <Switch>
+        <Route path='/blogs'>
 
-          </Route>
-          <Route path="/users">
-            <Message />
-            {
-              token.loggedInUserName === ''
-                ? <Toggleable buttonLabel={'Login'} >
-                  < LoginForm
-                    handleLogin={handleUserLogin}
-                  />
-                </Toggleable>
-                :
-                < LoggedInMessage
-                  loggedInUserName={token.loggedInUserName}
-                  loggedInName={token.loggedInName}
-                  handleLogout={handleLogout}
-                />
-            }
-            <UsersList />
-          </Route>
-          <Route path="/">
-            <Message />
-            {
-              token.loggedInUserName === ''
-                ? <Toggleable buttonLabel={'Login'} >
-                  < LoginForm
-                    handleLogin={handleUserLogin}
-                  />
-                </Toggleable>
-                :
-                < LoggedInMessage
-                  loggedInUserName={token.loggedInUserName}
-                  loggedInName={token.loggedInName}
-                  handleLogout={handleLogout}
-                />
-            }
-
-            {token.loggedInUserName !== null
-              ? <Toggleable buttonLabel={'send new blog'} ref={blogFormRef} >
-                < CreateBlogForm
-                  submitNewBlogToDb={submitNewBlogToDb}
+        </Route>
+        <Route path='/users/:id'>
+          {'asd'}
+        </Route>
+        <Route path='/users'>
+          <Message />
+          {
+            token.loggedInUserName === ''
+              ? <Toggleable buttonLabel={'Login'} >
+                < LoginForm
+                  handleLogin={handleUserLogin}
                 />
               </Toggleable>
-              : null
-            }
-            <div className='Padded-element'>
-              {blogs
-                .sort((a, b) => b.likes - a.likes)
-                .map(blog =>
-                  <Blog key={blog.id} blog={blog} likeBlog={handleBlogLike} deleteBlog={handleBlogDelete} />
-                )}
-            </div>
-          </Route>
-        </Switch>
-      </Router>
+              :
+              < LoggedInMessage
+                loggedInUserName={token.loggedInUserName}
+                loggedInName={token.loggedInName}
+                handleLogout={handleLogout}
+              />
+          }
+          <UsersList />
+        </Route>
+        <Route path="/">
+          <Message />
+          {
+            token.loggedInUserName === ''
+              ? <Toggleable buttonLabel={'Login'} >
+                < LoginForm
+                  handleLogin={handleUserLogin}
+                />
+              </Toggleable>
+              :
+              < LoggedInMessage
+                loggedInUserName={token.loggedInUserName}
+                loggedInName={token.loggedInName}
+                handleLogout={handleLogout}
+              />
+          }
+
+          {token.loggedInUserName !== null
+            ? <Toggleable buttonLabel={'send new blog'} ref={blogFormRef} >
+              < CreateBlogForm
+                submitNewBlogToDb={submitNewBlogToDb}
+              />
+            </Toggleable>
+            : null
+          }
+          <div className='Padded-element'>
+            {blogs
+              .sort((a, b) => b.likes - a.likes)
+              .map(blog =>
+                <Blog key={blog.id} blog={blog} likeBlog={handleBlogLike} deleteBlog={handleBlogDelete} />
+              )}
+          </div>
+        </Route>
+      </Switch>
     </div>
   )
 }
